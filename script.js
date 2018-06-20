f7429e05641a7b2775ae7320579da5a155f745f6
DylanHedges <d.hedges1@hotmail.co.uk>
2018-06-20 07:15:55 -0400

    Added JS script file, added Ajax request for new posts, added JQuery

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+$(document).ready(function(){
+  console.log(url);
+  //Executes a function when the submit button is clicked
+  $('#add-book').on('submit', function(e){
+    e.preventDefault();
+    //Stores form values in variables (linked by id '#'')
+    var title = $('#title').val();
+    var category = $('#category').val();
+    var excerpt = $('#excerpt').val();
 
+    $.ajax({ url: url,
+      data: JSON.stringify({
+        "title": title,
+        "category": category,
+        "excerpt": excerpt
+      }),
+      type: "POST",
+    	contentType: "application/json",
+      success: function(data){
+        window.location.href="index.html"
+      },
+      error: function(xhr, status, err){
+        console.log(err);
+      }
+     });
+  });
+});
